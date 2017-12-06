07aae52f57c4e94fc3a3435bf76f8cd0d5015a3b
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-06 15:05:01 +0000

    adds player name post request form

---
+++ spec/features/player_names_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../../lib/app'
+require 'capybara/rspec'
+
+feature "player names" do
+  scenario "player 1 enters name" do
+    visit('/')
+
+    fill_in :player1, with: "Varun"
+    fill_in :player2, with: "Ellie"
+    click_button "Confirm name"
+
+    expect(page).to have_content "Varun has joined the game as Duck 1 Ellie has joined the game as Duck 2"
+  end
+end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,6 +4,6 @@ require 'capybara/rspec'
 feature "Homepage content" do
   scenario "load content on page" do
     visit('/')
-    expect(page).to have_content "Testing infrastructure working!"
+    expect(page).to have_content "Duck 1 please enter your name:"
   end
 end
